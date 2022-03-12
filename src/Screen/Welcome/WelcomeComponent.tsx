36018b8618c776838bb1174bfbb0e629efb9a31e
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-03-13 00:54:35 +0700

    Create new screen

---
+++ src/Screen/Welcome/WelcomeComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import type { Props } from './types';
+
+/**
+ * WelcomeScreen
+ * @returns {React.FC<Props>} - Component
+ */
+const WelcomeScreen: React.FC<Props> = () => <Text>Hello</Text>;
+
+export default WelcomeScreen;
