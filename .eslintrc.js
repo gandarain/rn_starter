b72486bec04eba92318da19c692d7787420eb0dc
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-03-13 19:01:25 +0700

    setup ci/cd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  ignorePatterns: ['jest/*.ts'],
+  ignorePatterns: ['jest/*.ts', 'coverage/'],
   overrides: [
     {
       files: ['**/*.test.ts'],
