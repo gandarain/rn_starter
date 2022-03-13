c1c9d6fda21e4839ec3dccf24e15c5f5d5590260
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-03-13 13:48:31 +0700

    ### Add react native vector icons - Run this command yarn add react-native-vector-icons - Copy the icon from node_modules/react-native-vector-icons/Fonts to src/Assets/Icons - Add ./src/Assets/Icons inside assets on react-native.config.js - Add ./src/Assets/Icons inside assets on package.json - Run react-native link
    #### It will error while run on ios
    - Open project with xcode cd ios/open project-name.xcworkspace
    - Remove icons, open Build Phases, remove icons from Copy Bundle Resources

--- src/Screen/Welcome/Welcome.component.tsx
+++ src/Screen/Welcome/Welcome.component.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+import type { Props } from './types';
 import styles from './Welcome.styles';
 
 /**
  * WelcomeScreen
  * @returns {React.FC<Props>} - Component
  */
-const WelcomeScreen: React.ReactNode = () => (
+const WelcomeScreen: React.FC<Props> = (): JSX.Element => (
   <View style={styles.container}>
     <Text style={styles.title}>Hello</Text>
+    <Icon name="rocket" size={30} color="#900" />
   </View>
 );
 
